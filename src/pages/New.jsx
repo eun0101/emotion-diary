fe9c96d23b0a94197948e68f38ae4570e69c4a6f
정은아 <wjddmsdk21@onsecurity.co.kr>
2025-03-12 16:07:43 +0900

    [정은아] 1. New 페이지 구현하기 - UI

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,10 +1,18 @@
-const Diary = () => {
+import Header from "../components/Header.jsx";
+import Button from "../components/button.jsx";
+import Editor from "../components/Editor.jsx";
+import Edit from "./Edit.jsx";
+
+const New = () => {
 
     return (
         <div>
-            New
+            <Header title={"새 일기 쓰기"}
+            leftChild={<Button text={"< 뒤로 가기"}/>}></Header>
+
+            <Editor/>
         </div>
     );
 }
 
-export default Diary;
+export default New;
