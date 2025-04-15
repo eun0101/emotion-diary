5fd0b60457a1650d30adba8337296e634c5b7168
정은아 <wjddmsdk21@onsecurity.co.kr>
2025-04-15 15:41:15 +0900

    [정은아] 1. New 페이지 구현하기 - 기능

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,8 @@
 import "./Editor.css";
 import EmotionItem from "./EmotionItem.jsx";
 import Button from "./button.jsx";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 const emotionList = [
     {
@@ -25,14 +27,55 @@ const emotionList = [
     }
 ];
 
-const Editor = ()=>{
-    const emotionId = 1;
+const getStringedDate = (targetDate)=>{
+    // 날짜 =>YYYY-MM-DD
+    let [year, month, date] = [targetDate.getFullYear(),  targetDate.getMonth() +1,  targetDate.getDate()];
+
+    if (month < 10){
+        month = `0${month}`;
+    }
+    if (date < 10) {
+        date = `0${date}`;
+    }
+
+    return `${year}-${month}-${date}`;
+}
+
+
+const Editor = ({onSubmit})=>{
+    const [input, setInput] = useState({
+        createdDate: new Date(),
+        emotionId: 3,
+        content: "",
+    });
+    const nav = useNavigate();
+    const onChangeInput = (e) =>{
+
+        let name = e.target.name; //입력이 들어온 요소
+        let value = e.target.value; //입력된 값
+
+        if (name === "createdDate") {
+            value = new Date(value);
+        }
+
+        setInput({
+            ...input,
+            [name]: value
+        })
+    }
+
+    const onClickSubmitButton = () => {
+        onSubmit(input);
+    }
 
     return (
         <div className="Editor">
             <section className="date_section">
                 <h4>오늘의 날짜</h4>
-                <input type="date"/>
+                <input name="createdDate"
+                       value = {getStringedDate(input.createdDate)}
+                       type="date"
+                       onChange={onChangeInput}/>
             </section>
 
             <section className="emotion_section">
@@ -40,21 +83,34 @@ const Editor = ()=>{
                 <div className="emotion_list_wrapper">
                     {emotionList.map((item)=> (
                         //현재 선택 된 이모션 ID와 같은 값이라면 true 아니라면 false
-                        <EmotionItem key={item.emotionId}
+                        <EmotionItem onClick={()=>onChangeInput({
+                                        target: {
+                                            name: "emotionId",
+                                            value: item.emotionId
+                                        }
+                                    })}
+                                    key={item.emotionId}
                                      {...item}
-                                     isSelected={item.emotionId === emotionId}/>
+                                     isSelected={item.emotionId === input.emotionId}/>
                     ))}
                 </div>
             </section>
 
             <section className="content_section">
                 <h4>오늘의 일기</h4>
-                <textarea placeholder="오늘은 어땠나요?"></textarea>
+                <textarea name="content"
+                          value={input.content}
+                          onChange={onChangeInput}
+                          placeholder="오늘은 어땠나요?"></textarea>
             </section>
 
             <section className="button_section">
-                <Button text={"취소하기"}/>
-                <Button text={"작성 완료"} type={"POSITIVE"}/>
+                <Button
+                    onClick={()=> nav(-1)}
+                    text={"취소하기"}/>
+                <Button
+                    onClick={onClickSubmitButton}
+                    text={"작성 완료"} type={"POSITIVE"}/>
             </section>
         </div>
     );
--- src/components/EmotionItem.jsx
+++ src/components/EmotionItem.jsx
@@ -1,8 +1,9 @@
 import "./EmotionItem.css"
 import {getEmotionImage} from "../util/get-emotion-image.js";
-const EmotionItem = ({emotionId, emotionName, isSelected}) =>{
+const EmotionItem = ({emotionId, emotionName, isSelected, onClick}) =>{
  return (
-     <div className={`EmotionItem ${
+     <div onClick={onClick}
+         className={`EmotionItem ${
          isSelected ? `EmotionItem_on_${emotionId}` : ""
      }`}>
        <img className="emotion_img" src={getEmotionImage(emotionId)}/>
